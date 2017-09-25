36bca81e25a8134eef4312746f7a513c5ed3e147
unknown <mgdaye@gmail.com>
2017-09-25 19:14:34 -0400

    refactored html

---
+++ assets/app/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDeiIj5LHDVS9n0mr0qAGJZu4_5S9BvvLo",
+    authDomain: "mike-s-awesome-project.firebaseapp.com",
+    databaseURL: "https://mike-s-awesome-project.firebaseio.com",
+    projectId: "mike-s-awesome-project",
+    storageBucket: "mike-s-awesome-project.appspot.com",
+    messagingSenderId: "446038034983"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
