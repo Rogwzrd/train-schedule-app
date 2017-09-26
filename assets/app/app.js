fecc3a4d2e9f68040098952f6f29e382da04670c
unknown <mgdaye@gmail.com>
2017-09-25 22:48:16 -0400

    added a checklist of psuedo code comments

--- assets/app/app.js
+++ assets/app/app.js
@@ -11,29 +11,47 @@
 
   firebase.initializeApp(config);
 
+
   var database = firebase.database();
 
+  //variables for holding the train values
   var trainName = "",
       destination = "",
       trainTime = "",
-      frequency = 0;
+      frequency = "";
+      tillNextTrain = null;
 
+
+  //when you click on the submit button trun this funciton
   $("#submit").on("click", function(event) {
 
       event.preventDefault();
 
+      var firstTime = momemnt(trainTime);
+
+      //reassign the train values with the input from inputs on the page
       trainName = $("#train-name").val().trim();
       destination = $("#destination").val().trim();
-      trainTime = $("#train-time").val().trim();
+
+      //this is the first time of departure for the train
+      // trainTime = $("#train-time").val().trim();
+
+      //this is how long it takes for the train to arrive at it's destination
       frequency = $("#frequency").val().trim();
 
+      //this line needs to be an updating value based on the trainTime and the frequency, calculate the time between the frequency of the trains arrival and its departure time, this number will change with every cycle of the duration
+      // tillNextTrain = momemnt(trainTime) + momemnt()
+
+      //push the tnew train values to the database
       database.ref().push({
         trainName: trainName,
         destination: destination,
         trainTime: trainTime,
-        frequency: frequency
+        frequency: frequency,
+        tillNextTrain: tillNextTrain
       });
 
+      //empty the input areas
       $("#train-name").empty();
       $("#destination").empty();
       $("#train-time").empty();
@@ -41,20 +59,28 @@
   })
 
 
-
+  //when the pade loads or chrildren are added to the database this function runs
   database.ref().on("child_added", function(snapshot) {
     console.log(snapshot.val());
 
+    //create new table row element
     var newRow = $("<tr>"),
+
+        //create new table column elements and add values to them from the database
         newTrain = $("<td>").text(snapshot.val().trainName),
         newDestination = $("<td>").text(snapshot.val().destination),
         newTrainTime = $("<td>").text(snapshot.val().trainTime),
         newFrequency = $("<td>").text(snapshot.val().frequency);
+        newTillNextTrain = $("<td>").text(snapshot.val().tillNextTrain);
 
+        //appened the colums to the row
         newRow
         .append(newTrain)
         .append(newDestination)
         .append(newTrainTime)
-        .append(newFrequency);
+        .append(newFrequency),
+        .append(newTillNextTrain);
+
+        //append the row which is now holding columns elements to the table
         $("#train-table").append(newRow);
   })
